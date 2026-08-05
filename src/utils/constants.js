372fa4b4c488a240f9e1f18667a433e0702fe5cd
Git-Tutorial <mdsohail123g@gamil.com>
2026-08-05 14:49:46 +0530

    Make Base url dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,7 @@
-export const APP_BASE_URL = "/api";
+//  Production
+
+// export const APP_BASE_URL = "/api";
+
+// Development
+export const APP_BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
